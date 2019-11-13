ce5b3cd157280e56e6ad9154e9c1f82e19a44ffb
Rex Li <rex.li@class.com.au>
2019-11-13 17:15:13 +1100

    Add little-search app

---
+++ little-search/src/data/product.js
@@ -0,0 +1,10 @@
+const PRODUCTS = [
+    {category: 'Sporting Goods', price: '$9.99', hasStock: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$19.99', hasStock: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', hasStock: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', hasStock: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$199.99', hasStock: false, name: 'iPhone 10'},
+    {category: 'Electronics', price: '$299.99', hasStock: true, name: 'Nexus 7'}
+];
+
+export default PRODUCTS;
---
+++ little-search/src/index.js
@@ -0,0 +1,173 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import PRODUCTS from "./data/product.js";
+import * as serviceWorker from "./serviceWorker";
+
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filterText: "",
+      inStockOnly: false
+    };
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+
+  handleInStockChange(inStockOnly) {
+    this.setState({      
+      inStockOnly: inStockOnly
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.value);
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
+          />
+          Only Show Products in Stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach(product => {
+      if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+        return;
+      }
+
+      if (inStockOnly && !product.hasStock) {
+        return;
+      }
+
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category}
+          />
+        );
+      }
+
+      rows.push(<ProductRow product={product} key={product.name} />);
+
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class ProductCategoryRow extends React.Component {
+  render() {
+    const category = this.props.category;
+
+    return (
+      <tr>
+        <th colSpan="2">{category}</th>
+      </tr>
+    );
+  }
+}
+
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stock ? (
+      product.name
+    ) : (
+      <span style={{ color: "red" }}>{product.name}</span>
+    );
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById("root")
+);
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
